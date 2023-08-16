3cbbfbe08e0c91ff00ba53c2deab42843273b48e
Kim Jaejun <rlawowns97@gmail.com>
2023-08-17 02:05:06 +0900

    JB-16 : 한 유저의 총 여행 수 반환하는 API 추가

--- app/controllers/api/v1/journeys/journey_controller.rb
+++ app/controllers/api/v1/journeys/journey_controller.rb
@@ -68,6 +68,11 @@ class Api::V1::Journeys::JourneyController < ApplicationController
     render json: { code: 200, journey_count: Journey.all.count }
   end
 
+  def total_user_journey_count
+    user_journey_cnt = Journey.where(user_id: params[:user_id]).count
+    render json: { code: 200, journey_count: user_journey_cnt }
+  end
+
   private
 
   def already_traveling_or_preparing?
